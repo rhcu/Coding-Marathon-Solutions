22e4be688d1bd50f5722fb67f17f616223fe8533
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2018-09-21 16:27:15 +0600

    Create kangaroo.java

---
+++ kangaroo.java
@@ -0,0 +1,48 @@
+/*
+  Problem Statement: https://www.hackerrank.com/contests/nu-acm-w-coding-marathon/challenges/kangaroo
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // try to solve equation x1 + v1*t = x2 + v2*t => (x1-x2)%(v1-v2) = 0. 
+    // Edge case: if x1 < x2 and v1 < v2 the first kangaroo will never be able to reach the second
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        if(x1 < x2 && v1 < v2) return "NO";
+        if(v1!=v2 && Math.abs(x1-x2)%Math.abs(v1-v2)==0) return "YES";
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] x1V1X2V2 = scanner.nextLine().split(" ");
+
+        int x1 = Integer.parseInt(x1V1X2V2[0]);
+
+        int v1 = Integer.parseInt(x1V1X2V2[1]);
+
+        int x2 = Integer.parseInt(x1V1X2V2[2]);
+
+        int v2 = Integer.parseInt(x1V1X2V2[3]);
+
+        String result = kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
